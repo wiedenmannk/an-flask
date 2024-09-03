83ff6fc857258f858b5d518809146b2415f3f8ef
wiedenmannk <klaus.wiedenmann@yahoo.de>
2024-09-03 21:27:43 +0200

    generate zugferd pdf

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from flask import Flask, jsonify, request
 import pikepdf
 from lxml import etree
 
-
 app = Flask(__name__)
 
 # Konfiguriere das Logging nur für die Konsole
@@ -23,7 +22,6 @@ def get_data():
     text = f'Hello from Flask {random_number}'
     data = {"message": text}
     
-    # Logge den API-Call
     app.logger.info('GET /api/data - Response: %s', data)
     
     return jsonify(data)
@@ -33,35 +31,29 @@ def get_product(product_id):
     products = {
         1: { 'id': 1, 'name': 'Product 1', 'description': 'Description of Product 1', 'serial': generate_random_number() },
         2: { 'id': 2, 'name': 'Product 2', 'description': 'Description of Product 2', 'serial': generate_random_number() },
-        # Weitere Produkte können hier hinzugefügt werden
     }
     product = products.get(product_id)
     
     if product:
-        # Logge den API-Call und die Produktdaten
         app.logger.info('GET /api/product/%d - Response: %s', product_id, product)
         return jsonify(product)
     else:
         error_message = {'error': 'Product not found'}
-        # Logge den API-Call und den Fehler
         app.logger.info('GET /api/product/%d - Response: %s', product_id, error_message)
         return jsonify(error_message), 404
 
-## PDF Generator
 @app.route('/api/generate-zugferd', methods=['POST'])
 def generate_zugferd_pdf():
-    # Überprüfe, ob der Content-Type `application/json` ist
     if request.content_type != 'application/json':
         return jsonify({"error": "Unsupported Media Type"}), 415
 
-    # Versuche, die JSON-Daten zu extrahieren
     try:
         data = request.json
-        app.logger.info('data: %s', data)
+        app.logger.info('POST /api/generate-zugferd - Request Data: %s', data)
+
         pdf_path = data.get('pdf_path')
         xml_content = data.get('xml_content')
 
-        # Überprüfe, ob die erforderlichen Daten vorhanden sind
         if not pdf_path or not xml_content:
             return jsonify({"error": "Missing pdf_path or xml_content"}), 400
         
@@ -69,9 +61,12 @@ def generate_zugferd_pdf():
         pdf.attachments['ZUGFeRD-invoice.xml'] = pikepdf.EmbeddedFile(xml_content.encode(), name='ZUGFeRD-invoice.xml')
         output_path = pdf_path.replace(".pdf", "_zugferd.pdf")
         pdf.save(output_path, pdf_a_mode=pikepdf.PdfAFormat.PDF_A_3B)
+        
+        app.logger.info('POST /api/generate-zugferd - Response: %s', {"message": "ZUGFeRD PDF created successfully", "path": output_path})
         return jsonify({"message": "ZUGFeRD PDF created successfully", "path": output_path}), 200
 
     except Exception as e:
+        app.logger.error('POST /api/generate-zugferd - Error: %s', str(e))
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
