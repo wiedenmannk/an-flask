a805435434512936ed6ed051bbe2390238cb8fdc
wiedenmannk <klaus.wiedenmann@yahoo.de>
2024-09-09 20:15:38 +0200

    erste zugferd pdf erstellt

--- app.py
+++ app.py
@@ -10,41 +10,55 @@ app = Flask(__name__)
 # Konfiguriere das Logging nur für die Konsole
 logging.basicConfig(
     level=logging.DEBUG,
-    format='%(asctime)s - %(levelname)s - %(message)s',
-    handlers=[logging.StreamHandler()]
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    handlers=[logging.StreamHandler()],
 )
+
 
 def generate_random_number():
     return random.randint(1, 1000)
 
-@app.route('/api/data', methods=['GET'])
+
+@app.route("/api/data", methods=["GET"])
 def get_data():
     random_number = generate_random_number()
-    text = f'Hello from Flask {random_number}'
+    text = f"Hello from Flask {random_number}"
     data = {"message": text}
-    
-    app.logger.info('GET /api/data - Response: %s', data)
-    
+
+    app.logger.info("GET /api/data - Response: %s", data)
+
     return jsonify(data)
 
-@app.route('/api/products/<int:product_id>', methods=['GET'])
+
+@app.route("/api/products/<int:product_id>", methods=["GET"])
 def get_product(product_id):
     products = {
-        1: { 'id': 1, 'name': 'Product 1', 'description': 'Description of Product 1', 'serial': generate_random_number() },
-        2: { 'id': 2, 'name': 'Product 2', 'description': 'Description of Product 2', 'serial': generate_random_number() },
+        1: {
+            "id": 1,
+            "name": "Product 1",
+            "description": "Description of Product 1",
+            "serial": generate_random_number(),
+        },
+        2: {
+            "id": 2,
+            "name": "Product 2",
+            "description": "Description of Product 2",
+            "serial": generate_random_number(),
+        },
     }
     product = products.get(product_id)
-    
+
     if product:
-        app.logger.info('GET /api/product/%d - Response: %s', product_id, product)
+        app.logger.info("GET /api/product/%d - Response: %s", product_id, product)
         return jsonify(product)
     else:
-        error_message = {'error': 'Product not found'}
-        app.logger.info('GET /api/product/%d - Response: %s', product_id, error_message)
+        error_message = {"error": "Product not found"}
+        app.logger.info("GET /api/product/%d - Response: %s", product_id, error_message)
         return jsonify(error_message), 404
+
 
 # Registriere die Routen
 app.register_blueprint(zugferd_route)  # Hier registrierst du den Blueprint direkt
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
--- tests/test_pdf_xml.py
+++ tests/test_pdf_xml.py
@@ -1,21 +1,27 @@
 import os
-from pathlib import os
+import sys
 from pathlib import Path
+
+# Füge das Projekt-Root-Verzeichnis zu sys.path hinzu
+root_dir = Path(__file__).parent.parent
+sys.path.append(str(root_dir))
+
 from service.pdf_maker import PDFMaker
 from service.zugferd_xml_generator import ZugferdXmlGenerator
 
-# Globale Variable für das Root-Verzeichnis
-root_dir = Path(__file__).parent.parent
 
+# Globale Variable für das Root-Verzeichnis
 def test_pdf_processing():
     print(f"Root directory: {root_dir}")
 
     # Pfade definieren
     pdf_path = root_dir / "files" / "test.pdf"
+    base64_pdf_path = root_dir / "files" / "pdf_base64.txt"
     xml_filepath = root_dir / "generated" / "zugferd.xml"
     output_pdf_path = root_dir / "generated" / "output_with_xml.pdf"
 
     print(f"PDF Path: {pdf_path}")
+    print(f"Base64 PDF Path: {base64_pdf_path}")
     print(f"XML Path: {xml_filepath}")
     print(f"Output PDF Path: {output_pdf_path}")
 
@@ -26,7 +32,7 @@ def test_pdf_processing():
     json_data = {
         "InvoiceNumber": "12345",
         "InvoiceDate": "2024-09-06",
-        "Amount": "100.00"
+        "Amount": "100.00",
     }
 
     # ZUGFeRD XML-Generator erstellen und XML generieren
@@ -36,12 +42,19 @@ def test_pdf_processing():
     xml_string = xml_generator.generate_xml_from_json(json_data, xml_filepath)
     print(f"Generated XML String:\n{xml_string}")
 
+    # Base64-kodiertes PDF aus der Datei lesen
+    with open(base64_pdf_path, "r") as file:
+        base64_pdf = file.read()
+
     # PDFMaker erstellen und XML an PDF anhängen
     pdf_maker = PDFMaker()
-    pdf = pdf_maker.process_base64_pdf("JVBERi0xLjcKJc...")  # Ersetze dies durch echten Base64-Daten oder lade PDF direkt
+    pdf = pdf_maker.process_base64_pdf(base64_pdf)
     if pdf is not None:
-        pdf_maker.attach_xml(str(xml_filepath), "zugferd.xml", str(output_pdf_path), pdf)
+        pdf_maker.attach_xml(
+            str(xml_filepath), "zugferd.xml", str(output_pdf_path), pdf
+        )
         print(f"PDF with XML attached saved to: {output_pdf_path}")
+
 
 # Test ausführen
 if __name__ == "__main__":
